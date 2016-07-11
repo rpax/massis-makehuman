19e7a3e1b26cf8eff7a0cc89440b23b13eb73ef2
Rafael Pax <rpax@ucm.es>
2016-07-11 15:20:02 +0200

    Trigger again

--- massis-makehuman/src/main/java/com/massisframework/makehuman/BlenderConverter.java
+++ massis-makehuman/src/main/java/com/massisframework/makehuman/BlenderConverter.java
@@ -54,7 +54,7 @@ public class BlenderConverter extends SimpleApplication {
 				Node n = (Node) assetManager.loadModel(modelP);
 				n = prepare(n);
 
-				// Guardamos.
+				
 				String baseName = FilenameUtils.getBaseName(modelP);
 				File realModelFile = outputPath.resolve(modelP).getParent()
 						.resolve(baseName + ".j3o").toFile();
