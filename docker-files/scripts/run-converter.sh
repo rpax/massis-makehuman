85d22ec715e90cecccde6d2e28f57ae8ba7a593f
Rafael Pax <rpax@ucm.es>
2017-02-15 16:19:15 +0100

    added multithread capability

--- docker-files/scripts/run-converter.sh
+++ docker-files/scripts/run-converter.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # dont use hw acc
 export LIBGL_ALWAYS_SOFTWARE=1
+
+if [[ "$1" != "" ]]; then
+    nThreads="$1"
+else
+    nThreads=1
+fi
+
 rm -rf /output/*
-/usr/local/blender/blender-softwaregl --background --python /usr/bin/mixamo_to_blender.py
+find /input -name "*.mhx2" \
+| xargs realpath \
+| xargs -n 1 -P "$nThreads" /usr/local/blender/blender-softwaregl --background --python /usr/bin/mixamo_to_blender.py
 chmod -R 777 /output/
 mvn exec:java -Dexec.mainClass="com.massisframework.makehuman.BlenderConverter" -Dmassis3.workingDir="/output"
