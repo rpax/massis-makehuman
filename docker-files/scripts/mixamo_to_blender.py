1aed2f68f36fc1eb704a42f059ff347c5270e782
Rafael Pax <rpax@ucm.es>
2017-03-11 14:56:51 +0100

    Reverted to prev version

--- docker-files/scripts/mixamo_to_blender.py
+++ docker-files/scripts/mixamo_to_blender.py
@@ -34,61 +34,60 @@ def animfiles(gender):
 
 
 
-#for mhx2_file in findfrec("/input",'mhx2'):
-mhx2_file=sys.argv[-1]
-bpy.ops.wm.read_homefile()
-print("Processing "+mhx2_file)
-clearscene()
-prepareaddons()
-mhxgender=os.path.splitext(os.path.basename(mhx2_file))[0].split('-')[0]
-bpy.ops.import_scene.makehuman_mhx2(filepath=mhx2_file)
-added_items=[]
-for name in animfiles(mhxgender):
-	bpy.ops.mcp.load_and_retarget(filepath=name)
-	for element in bpy.data.actions:
-		if element not in added_items:
-			print("Adding "+element.name)
-			element.name=os.path.splitext(os.path.basename(name))[0]
-			added_items.append(element)
-			break
-#bpy.ops.mcp.loop_fcurves()
-#bpy.ops.mcp.repeat_fcurves()
-bpy.data.objects[0].scale=mathutils.Vector((0.1000000, 0.100000, 0.100000))
+for mhx2_file in findfrec("/input",'mhx2'):
+	bpy.ops.wm.read_homefile()
+	print("Processing "+mhx2_file)
+	clearscene()
+	prepareaddons()
+	mhxgender=os.path.splitext(os.path.basename(mhx2_file))[0].split('-')[0]
+	bpy.ops.import_scene.makehuman_mhx2(filepath=mhx2_file)
+	added_items=[]
+	for name in animfiles(mhxgender):
+		bpy.ops.mcp.load_and_retarget(filepath=name)
+		for element in bpy.data.actions:
+			if element not in added_items:
+				print("Adding "+element.name)
+				element.name=os.path.splitext(os.path.basename(name))[0]
+				added_items.append(element)
+				break
+	#bpy.ops.mcp.loop_fcurves()
+	#bpy.ops.mcp.repeat_fcurves()
+	bpy.data.objects[0].scale=mathutils.Vector((0.1000000, 0.100000, 0.100000))
 
-# remove specular int
-for m in bpy.data.materials : m.specular_intensity=0.0
+	# remove specular int
+	for m in bpy.data.materials : m.specular_intensity=0.0
 
-print("joining geometries....")
-scene = bpy.context.scene
-obs = []
-for ob in scene.objects:
-	# whatever objects you want to join...
-	if ob.type == 'MESH':
-	    obs.append(ob)
-ctx = bpy.context.copy()
-# one of the objects to join
-ctx['active_object'] = obs[0]
-ctx['selected_objects'] = obs
-# we need the scene bases as well for joining
-ctx['selected_editable_bases'] = [scene.object_bases[ob.name] for ob in obs]
-bpy.ops.object.join(ctx)
+	print("joining geometries....")
+	scene = bpy.context.scene
+	obs = []
+	for ob in scene.objects:
+		# whatever objects you want to join...
+		if ob.type == 'MESH':
+		    obs.append(ob)
+	ctx = bpy.context.copy()
+	# one of the objects to join
+	ctx['active_object'] = obs[0]
+	ctx['selected_objects'] = obs
+	# we need the scene bases as well for joining
+	ctx['selected_editable_bases'] = [scene.object_bases[ob.name] for ob in obs]
+	bpy.ops.object.join(ctx)
 
-#for k in filter(lambda ob: ob.type=='MESH',bpy.data.objects):
-#	ctx = bpy.context.copy()
-#	ctx['active_object']=k
-#	bpy.ops.object.modifier_add(type='TRIANGULATE')
-#	k.modifiers['Triangulate'].quad_method='BEAUTY'
-#	bpy.ops.object.modifier_apply(apply_as='DATA',modifier='Triangulate')
+	#for k in filter(lambda ob: ob.type=='MESH',bpy.data.objects):
+	#	ctx = bpy.context.copy()
+	#	ctx['active_object']=k
+	#	bpy.ops.object.modifier_add(type='TRIANGULATE')
+	#	k.modifiers['Triangulate'].quad_method='BEAUTY'
+	#	bpy.ops.object.modifier_apply(apply_as='DATA',modifier='Triangulate')
 
-ftree="/".join(os.path.splitext(mhx2_file)[0].split('/')[2:-1])
-print("ftree: "+ftree)
-targetfolder = '/output/'+ ftree
-os.makedirs(targetfolder,exist_ok=True)
-blendfilename=os.path.splitext(os.path.basename(mhx2_file))[0]+'.blend'
-print("bfn: "+blendfilename)
-bpy.ops.file.pack_all()
-bpy.ops.wm.save_as_mainfile(filepath=targetfolder+'/'+blendfilename)
-bpy.ops.file.unpack_all(method='WRITE_LOCAL')
+	ftree="/".join(os.path.splitext(mhx2_file)[0].split('/')[2:-1])
+	print("ftree: "+ftree)
+	targetfolder = '/output/'+ ftree
+	os.makedirs(targetfolder)
+	blendfilename=os.path.splitext(os.path.basename(mhx2_file))[0]+'.blend'
+	print("bfn: "+blendfilename)
+	bpy.ops.file.pack_all()
+	bpy.ops.wm.save_as_mainfile(filepath=targetfolder+'/'+blendfilename)
+	bpy.ops.file.unpack_all(method='WRITE_LOCAL')
 
 #######################################
 
--- docker-files/scripts/run-converter.sh
+++ docker-files/scripts/run-converter.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 # dont use hw acc
 export LIBGL_ALWAYS_SOFTWARE=1
-
-if [[ "$1" != "" ]]; then
-    nThreads="$1"
-else
-    nThreads=1
-fi
-
-echo "Running with $nThreads threads"
-
 rm -rf /output/*
-find /input -name "*.mhx2" \
-| xargs realpath \
-| xargs -n 1 -P "$nThreads" /usr/local/blender/blender-softwaregl --background --python /usr/bin/mixamo_to_blender.py
+/usr/local/blender/blender-softwaregl --background --python /usr/bin/mixamo_to_blender.py
 chmod -R 777 /output/
 mvn exec:java -Dexec.mainClass="com.massisframework.makehuman.BlenderConverter" -Dmassis3.workingDir="/output"
